baffdd4214252a91e3d187d2b79e3b78bf5502ed
Rizqi <rizqisyaban2510@gmail.com>
2025-11-27 23:31:39 +0700

    Update Lanjutan

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:encrypt/encrypt.dart' as encrypt;
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -12,18 +13,26 @@ class _ProfileScreenState extends State<ProfileScreen> {
   // 1. Declare necessary variables
   bool isSignedIn = false;
   String fullName = ''; // Example name
-  String userName = ''; // Example username
+  String userName = ""; // Example username
   int favoriteCandiCount = 0;
+  late Color iconColor;
 
   //5. implementasi fungsi signIn
   void signIn() {
     Navigator.pushNamed(context, "/signin");
+    // setState(() {
+    //   isSignedIn = !isSignedIn;
+    //   userName = 'faiz';
+    //   fullName = 'Faiz Ganteng';
+    //   favoriteCandiCount = 3;
+    // });
   }
 
   //6. implementasi fungsi signOut
   void signOut() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.setBool('isSignedIn', false);
+
     //await prefs.remove('username');
     //await prefs.remove('name');
 
@@ -42,19 +51,27 @@ class _ProfileScreenState extends State<ProfileScreen> {
   }
 
   void _identitas() async {
+
     SharedPreferences prefs = await SharedPreferences.getInstance();
     setState(() {
       fullName = prefs.getString("fullname") ?? "";
       userName = prefs.getString("username") ?? "";
     });
+    final keyString = prefs.getString('key') ?? '';
+    final ivString = prefs.getString('iv') ?? '';
+    final encrypt.Key key = encrypt.Key.fromBase64(keyString);
+    final iv = encrypt.IV.fromBase64(ivString);
+
+    final encrypter = encrypt.Encrypter(encrypt.AES(key));
+    fullName = encrypter.decrypt64(fullName, iv: iv);
+    userName = encrypter.decrypt64(userName, iv: iv);
   }
 
   @override
   void initState() {
     _checkSignInStatus();
-    if (isSignedIn) {
-      _identitas();
-    }
+    _identitas();
+
     super.initState();
   }
 
@@ -90,7 +107,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                           child: CircleAvatar(
                             radius: 50,
                             backgroundImage: AssetImage(
-                              'images/placeholder_image.png',
+                              'assets/placeholder_image.png',
                             ),
                           ),
                         ),
